548cb689cd80d85415f74ae15a2a2de1c325f700
Nicolas Salles <nsalles33@gmail.com>
2018-01-26 00:31:14 +0100

    Typo

--- src/Kernel.f90
+++ src/Kernel.f90
@@ -22,58 +22,6 @@
 !  - I try to design the program in object oriented... may be is not convinient... I try
 !  
 ! =================================================================================================
-  module derived_types
-    implicit none
-
-    private 
-    public :: constructor, destructor, print_state
-
-    type, public :: KMC_type
-      logical                            :: bavard
-      integer                            :: nsites
-      real                               :: sum_rate, t
-      integer, dimension(*), allocatable :: site
-      real, dimension(*), allocatable    :: rate
-     contains
-      procedure :: print => print_kmc_type
-    end type KMC_type
-
-    contains
-    
-    subroutine constructor( this, a )
-      implicit none
-      type( KMC_type ) :: this
-      integer, intent( in ) :: a
-      this% t = 0
-      this% sum_rate = 0
-      this% nsites = a
-      this% bavard = .false.
-      allocate( this% site(a), this% rate(a) )
-      if ( .not.allocated(this% site) ) print*, " CONSTRUCTOR problem..."
-      if ( .not.allocated(this% rate) ) print*, " CONSTRUCTOR problem..."
-    end subroutine constructor
-! ............................................................................
-    subroutine destructor ( this )
-      implicit none
-      type( KMC_type ) :: this
-      deallocate( this% site, this% rate )
-      if ( allocated(this% site) ) print*, " DESTRUCTOR problem ..."
-      if ( allocated(this% rate) ) print*, " DESTRUCTOR problem ..."
-    end subroutine destructor
-! ............................................................................
-    subroutine print_kmc_type ( this )
-      implicit none
-      type( KMC_type ) :: this
-      print*, " - To do :: print_kmc_type "
-    end subroutine print_kmc_type
-! ............................................................................
-    subroutine print_state( this )
-      implicit none
-      type( KMC_type ) :: this
-      print*, " - To do :: print_state "
-    end subroutine print_state
-  end module derived_types
-! ============================================================================
 
   Program Kernel
 !
